a2d3ce3a2788a6cb294d259a0862e429ab88f13c
unknown <tim.mota21@gmail.com>
2024-08-24 22:30:09 +1000

    Dynamic Selected Collection feature

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import SelectedItemVideo from "../../assets/selected-collection.mp4";
-import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
+import React, { useEffect, useState } from "react";
 import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
 import VerifiedIcon from "../../assets/verified.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import SkeletonSelectedCollection from "./SelectedCollectionSkeleton"
 
 export default function SelectedCollection() {
+
+  const [data, setData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/selectedCollection"
+      );
+      setData(response.data.data)
+    } catch (error) {
+      alert("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <SkeletonSelectedCollection />; // Render skeleton while data is loading
+  }
+
+
   return (
     <header>
       <div className="selected-collection">
@@ -14,8 +37,8 @@ export default function SelectedCollection() {
           muted
           loop
           playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
+          poster={data?.thumbnail}
+          src={data?.videoLink}
           className="selected-collection__bg"
         />
         <div className="selected-collection__description">
@@ -25,17 +48,17 @@ export default function SelectedCollection() {
             className="selected-collection__logo"
           />
           <h1 className="selected-collection__title">
-            Coachella Throwback Merchandise Trunk
+            {data?.title || "Loading..."}
           </h1>
-          <Link to={'/user'} className="selected-collection__author">
-            By Coachella
+          <Link to={"/user"} className="selected-collection__author">
+            By {data?.creator || "Loading..."}
             <img
               src={VerifiedIcon}
               className="selected-collection__author__verified"
             />
           </Link>
-          <div className="selected-collection__details">10 items · 3.4 ETH</div>
-          <Link to={'/collection'} className="selected-collection__button">
+          <div className="selected-collection__details">{data?.amountOfItems} Items · {data?.floorPrice} ETH</div>
+          <Link to={"/collection"} className="selected-collection__button">
             <div className="green-pulse"></div>
             View Collection
           </Link>
